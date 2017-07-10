18aef880ea5588ac1a670f454ac6f2496edd3ba3
Vadim Bulavin <vadim.bulavin@akvelon.com>
2017-07-10 13:37:06 +0300

    Added MSGP-Example target for SDK integration purposes.

---
+++ MSGP-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSGP-Example
+//
+//  Created by Vadim Bulavin on 7/10/17.
+//  Copyright © 2017 Igor Popov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
