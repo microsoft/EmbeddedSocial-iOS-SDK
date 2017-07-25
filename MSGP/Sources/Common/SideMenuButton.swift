0ecdc80afbf37cd1dccf84224252f6750aa34a2f
Igor Popov <v-igpopo@microsoft.com>
2017-07-25 13:51:56 +0300

    Fixed header.

--- MSGP/Sources/Common/SideMenuButton.swift
+++ MSGP/Sources/Common/SideMenuButton.swift
@@ -1,9 +1,6 @@
 //
-//  SideMenuButton.swift
-//  MSGP
-//
-//  Created by Igor Popov on 7/24/17.
-//  Copyright © 2017 Akvelon. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 //
 
 class SideMenuButton: UIButton {
