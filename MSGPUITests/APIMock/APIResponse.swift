3635e9930727e62e819ea8571e7b01a12a7cdddf
Jaroslaw Romanovich <jaroslaw.romanovich@akvelon.com>
2017-07-28 02:52:31 +0300

    Mock API for post creation and post creation test

--- MSGPUITests/APIMock/APIResponse.swift
+++ MSGPUITests/APIMock/APIResponse.swift
@@ -5,49 +5,79 @@
 
 import Foundation
 import EnvoyAmbassador
+import Embassy
 
-func APIResponse(
-    statusCode: Int = 200,
-    statusMessage: String = "OK",
-    contentType: String = "application/json",
-    jsonWritingOptions: JSONSerialization.WritingOptions = .prettyPrinted,
-    headers: [(String, String)] = [],
-    handler: @escaping (_ environ: [String: Any], _ sendJSON: @escaping (Any) -> Void) -> Void) -> Any
-{
-    let response = JSONResponse(
-        statusCode: statusCode,
-        statusMessage: statusMessage,
-        contentType: contentType,
-        jsonWritingOptions: jsonWritingOptions,
-        headers: headers,
-        handler: handler)
+public struct APIResponse: WebApp {
+    let dataResponse: WebApp
     
-    if APIConfig.delayedResponses {
-        return DelayResponse(response)
-    } else {
-        return response
+    public init(
+        serviceName: String = "generic",
+        statusCode: Int = 200,
+        statusMessage: String = "OK",
+        contentType: String = "application/json",
+        jsonWritingOptions: JSONSerialization.WritingOptions = .prettyPrinted,
+        headers: [(String, String)] = [],
+        handler: @escaping (_ environ: [String: Any], _ sendJSON: @escaping (Any) -> Void) -> Void
+        ) {
+        let response = DataResponse(
+            statusCode: statusCode,
+            statusMessage: statusMessage,
+            contentType: contentType,
+            headers: headers
+        ) { environ, sendData in
+            handler(environ) { json in
+                let data = try! JSONSerialization.data(withJSONObject: json, options: jsonWritingOptions)
+                APIState.setLatestResponseAsString(forService: serviceName, data: String(data: data, encoding: .utf8) as String!)
+                sendData(data)
+            }
+        }
+        
+        if APIConfig.delayedResponses {
+            dataResponse = DelayResponse(response)
+        } else {
+            dataResponse = response
+        }
+       
     }
-}
-
-func APIResponse(
-    statusCode: Int = 200,
-    statusMessage: String = "OK",
-    contentType: String = "application/json",
-    jsonWritingOptions: JSONSerialization.WritingOptions = .prettyPrinted,
-    headers: [(String, String)] = [],
-    handler: ((_ environ: [String: Any]) -> Any)? = nil) -> WebApp
-{
-    let response = JSONResponse(
-        statusCode: statusCode,
-        statusMessage: statusMessage,
-        contentType: contentType,
-        jsonWritingOptions: jsonWritingOptions,
-        headers: headers,
-        handler: handler)
     
-    if APIConfig.delayedResponses {
-        return DelayResponse(response)
-    } else {
-        return response
+    public init(
+        serviceName: String = "generic",
+        statusCode: Int = 200,
+        statusMessage: String = "OK",
+        contentType: String = "application/json",
+        jsonWritingOptions: JSONSerialization.WritingOptions = .prettyPrinted,
+        headers: [(String, String)] = [],
+        handler: ((_ environ: [String: Any]) -> Any)? = nil
+        ) {
+        let response = DataResponse(
+            statusCode: statusCode,
+            statusMessage: statusMessage,
+            contentType: contentType,
+            headers: headers
+        ) { environ, sendData in
+            let data: Data
+            if let handler = handler {
+                let json = handler(environ)
+                data = try! JSONSerialization.data(withJSONObject: json, options: jsonWritingOptions)
+            } else {
+                data = Data()
+            }
+            APIState.setLatestResponseAsString(forService: serviceName, data: String(data: data, encoding: .utf8) as String!)
+            sendData(data)
+        }
+        
+        if APIConfig.delayedResponses {
+            dataResponse = DelayResponse(response)
+        } else {
+            dataResponse = response
+        }
+    }
+    
+    public func app(
+        _ environ: [String: Any],
+        startResponse: @escaping ((String, [(String, String)]) -> Void),
+        sendBody: @escaping ((Data) -> Void)
+        ) {
+        return dataResponse.app(environ, startResponse: startResponse, sendBody: sendBody)
     }
 }
