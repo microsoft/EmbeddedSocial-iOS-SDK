589fe671a49a5e0df0056301adcf421ff452d6b4
Igor Popov <igor.popov@akvelon.com>
2017-07-31 16:28:38 +0300

    Wip.

--- EmbeddedSocial/Sources/Modules/Home/Interactor/HomeInteractor.swift
+++ EmbeddedSocial/Sources/Modules/Home/Interactor/HomeInteractor.swift
@@ -60,8 +60,6 @@ class PostServiceMock: PostServiceProtocol {
     }
 }
 
-typealias PostHandle = String
-
 protocol HomeInteractorInput {
     
     func fetchPosts(with limit: Int)
--- EmbeddedSocial/Sources/Modules/Home/Presenter/HomeModuleInput.swift
+++ EmbeddedSocial/Sources/Modules/Home/Presenter/HomeModuleInput.swift
@@ -3,6 +3,29 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 //
 
+typealias PostHandle = String
+
+enum Feed {
+    
+    enum PopularType {
+        case today, weekly, alltime
+    }
+    
+    // Shows recent feed
+    case recent
+    // Shows popular feed
+    case popular(type: PopularType)
+    // Shows single post
+    case single(post: PostHandle)
+}
+
 protocol HomeModuleInput: class {
+    
+    var configuration: Feed
+    
+//    var needsShowRecentPosts { set get }
+//    var needsShowPopularPosts { set get }
+    
+    
     
 }
