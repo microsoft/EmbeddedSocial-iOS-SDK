fdf462a74b118683dd6a1aec8dc938916803e710
Vadim Bulavin <vadim.bulavin@akvelon.com>
2017-08-15 10:57:29 +0300

    In progress

---
+++ EmbeddedSocial/Sources/Modules/EditProfile/EditProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditProfileViewController.swift
+//  EmbeddedSocial
+//
+//  Created by Vadim Bulavin on 8/15/17.
+//  Copyright © 2017 Akvelon. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
