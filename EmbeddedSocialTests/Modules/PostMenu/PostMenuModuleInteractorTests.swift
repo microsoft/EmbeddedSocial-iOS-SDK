11e6865a07606f8c62a3edc0c82cb5e0acbfc0e0
Igor Popov <igor.popov@akvelon.com>
2017-08-17 12:14:45 +0300

    Wip.

---
+++ EmbeddedSocialTests/Modules/PostMenu/PostMenuModuleInteractorTests.swift
@@ -0,0 +1,21 @@
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+
+import XCTest
+@testable import EmbeddedSocial
+
+class PostMenuModuleInteractorTests: XCTestCase {
+    
+    var sut: PostMenuModuleInteractorTests!
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func test() {
+        
+    }
+    
+}
---
+++ EmbeddedSocialTests/Modules/PostMenu/PostMenuModulePresenterTests.swift
@@ -0,0 +1,22 @@
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+
+import XCTest
+@testable import EmbeddedSocial
+
+class PostMenuModulePresenterTests: XCTestCase {
+    
+    var sut: PostMenuModulePresenter!
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func test() {
+        
+    }
+    
+}
+
