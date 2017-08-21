fd8ac3c2608b4b78441fe9d372578577547b747a
Igor Popov <igor.popov@akvelon.com>
2017-08-21 14:31:37 +0300

    Added test for presenter.

---
+++ EmbeddedSocialTests/Modules/Popular/PopularModulePresenterTests.swift
@@ -0,0 +1,21 @@
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+
+import XCTest
+@testable import EmbeddedSocial
+
+private class PopularModulePresenterTests: XCTestCase {
+    
+    var sut: PopularModulePresenter!
+    
+    override func setUp() {
+        super.setUp()
+        
+        sut = PopularModulePresenter()
+        
+        
+    }
+
+}
