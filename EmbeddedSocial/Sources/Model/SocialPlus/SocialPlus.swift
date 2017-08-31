808934ad90c3efb62e67a5cff2701444c79f5233
Vadim Bulavin <vadim.bulavin@akvelon.com>
2017-08-31 15:07:14 +0300

    In progress

--- EmbeddedSocial/Sources/Model/SocialPlus/SocialPlus.swift
+++ EmbeddedSocial/Sources/Model/SocialPlus/SocialPlus.swift
@@ -53,21 +53,6 @@ public final class SocialPlus {
         } else {
             coordinator.openPopularScreen()
         }
-        
-        let service = SocialService()
-        
-        service.getMyFollowers(cursor: nil, limit: 30) { result in
-            withExtendedLifetime(service) {
-                print(result)
-            }
-        }
-        
-//        let service = LikesService()
-//        service.getPostLikes(postHandle: "3usUWglABdh", cursor: nil, limit: 30) { result in
-//            withExtendedLifetime(service) {
-//                print(result)
-//            }
-//        }
     }
     
     private func setupCoreDataStack() {
