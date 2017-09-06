5be76774b55e5d256b6a5b35b8c7120dd06a8ab7
Vadim Bulavin <vadim.bulavin@akvelon.com>
2017-09-06 18:35:33 +0300

    In progress

--- EmbeddedSocial/Sources/Modules/Search/View/SearchViewController.swift
+++ EmbeddedSocial/Sources/Modules/Search/View/SearchViewController.swift
@@ -10,7 +10,11 @@ class SearchViewController: UIViewController {
     
     var output: SearchViewOutput!
 
-    fileprivate var searchController: UISearchController?
+    fileprivate var peopleSearchController: UISearchController?
+    fileprivate var peopleSearchText: String?
+    
+    fileprivate var topicsSearchController: UISearchController?
+    fileprivate var topicsSearchText: String?
     
     fileprivate lazy var filterView: SegmentedControlView = { [unowned self] in
         return SegmentedControlView.searchModuleControl(
@@ -30,22 +34,64 @@ class SearchViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        searchController?.isActive = false
+//        activeSearchController?.isActive = false
     }
     
-    fileprivate func setupSearchController(_ tabInfo: SearchTabInfo) {
-        searchController = UISearchController(searchResultsController: tabInfo.searchResultsController)
-        searchController?.searchResultsUpdater = tabInfo.searchResultsHandler
-        searchController?.dimsBackgroundDuringPresentation = false
-        searchController?.searchBar.delegate = self
-        searchController?.hidesNavigationBarDuringPresentation = false
-        searchController?.searchBar.searchBarStyle = .minimal
-        searchController?.searchBar.placeholder = tabInfo.tab.searchBarPlaceholder
-        searchController?.delegate = self
+    fileprivate func makeTabActive(_ tab: SearchTabInfo) {
+        if tab.tab == .topics {
+            setupTopicsSearchController(with: tab)
+        } else if tab.tab == .people {
+            setupPeopleSearchController(with: tab)
+        }
+        if let backgroundView = tab.backgroundView {
+            addBackgroundView(backgroundView)
+        }
+    }
+    
+    private func setupPeopleSearchController(with tab: SearchTabInfo) {
+        if peopleSearchController == nil {
+            peopleSearchController = makeSearchController(with: tab)
+        } else {
+            peopleSearchController?.isActive = true
+        }
+        navigationItem.titleView = peopleSearchController?.searchBar
+        navigationItem.rightBarButtonItem = nil
+        peopleSearchController?.searchBar.text = peopleSearchText
+    }
+    
+    private func setupTopicsSearchController(with tab: SearchTabInfo) {
+        if topicsSearchController == nil {
+            topicsSearchController = makeSearchController(with: tab)
+        } else {
+            topicsSearchController?.isActive = true
+        }
         
-        navigationItem.titleView = searchController?.searchBar
-        navigationItem.rightBarButtonItem =
-            tabInfo.tab.showsRightNavigationButton ? UIBarButtonItem(customView: feedLayoutButton) : nil
+        navigationItem.titleView = topicsSearchController?.searchBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: feedLayoutButton)
+        topicsSearchController?.searchBar.text = self.topicsSearchText
+    }
+    
+    fileprivate func makeSearchController(with tabInfo: SearchTabInfo) -> UISearchController {
+        let searchController = UISearchController(searchResultsController: tabInfo.searchResultsController)
+        searchController.searchResultsUpdater = tabInfo.searchResultsHandler
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.searchBarStyle = .minimal
+        searchController.searchBar.placeholder = tabInfo.tab.searchBarPlaceholder
+        searchController.delegate = self
+        return searchController
+    }
+    
+    fileprivate func hideTab(_ tab: SearchTabInfo) {
+        if tab.tab == .topics {
+            topicsSearchController?.isActive = false
+        } else if tab.tab == .people {
+            peopleSearchController?.isActive = false
+        }
+        if let backgroundView = tab.backgroundView {
+            backgroundView.removeFromSuperview()
+        }
     }
     
     fileprivate func addBackgroundView(_ backgroundView: UIView) {
@@ -68,25 +114,8 @@ extension SearchViewController: SearchViewInput {
     }
     
     func switchTabs(to tab: SearchTabInfo, from previousTab: SearchTabInfo) {
-        removeTab(previousTab)
+        hideTab(previousTab)
         makeTabActive(tab)
-    }
-    
-    private func makeTabActive(_ tab: SearchTabInfo) {
-        setupSearchController(tab)
-        if let backgroundView = tab.backgroundView {
-            addBackgroundView(backgroundView)
-        }
-    }
-    
-    private func removeTab(_ tab: SearchTabInfo) {
-        if let searchController = searchController {
-            searchController.isActive = false
-            searchController.removeFromParentViewController()
-        }
-        if let backgroundView = tab.backgroundView {
-            backgroundView.removeFromSuperview()
-        }
     }
     
     func showError(_ error: Error) {
@@ -104,11 +133,27 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if peopleSearchController?.isActive == true {
+            peopleSearchText = searchText
+        } else if topicsSearchController?.isActive == true {
+            topicsSearchText = searchText
+        }
+    }
 }
 
 extension SearchViewController: UISearchControllerDelegate {
+    
+//    func willPresentSearchController(_ searchController: UISearchController) {
+//        updateSearchControllerFrame(searchController)
+//    }
 
     func didPresentSearchController(_ searchController: UISearchController) {
+        updateSearchControllerFrame(searchController)
+    }
+    
+    private func updateSearchControllerFrame(_ searchController: UISearchController) {
         var frame = searchController.view.frame
         frame.origin.y += Constants.Search.filterHeight
         frame.size.height -= Constants.Search.filterHeight
