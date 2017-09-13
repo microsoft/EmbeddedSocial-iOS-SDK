18e8775281c9bde6abe4a89d482c0d8d169bd41b
Jaroslaw Romanovich <jaroslaw.romanovich@akvelon.com>
2017-09-14 02:05:20 +0300

    Loop test commented

--- EmbeddedSocialUITests/Tests/Loop.swift
+++ EmbeddedSocialUITests/Tests/Loop.swift
@@ -7,37 +7,20 @@ import XCTest
 
 //Uncomment and run to test and debug API Mock
 //
-class Loop: UITestBase {
-    
-    override func setUp() {
-        super.setUp()
-        
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-    
-    func testAPIMock() {
-        while true {}
-        
-//        let collectionViewsQuery = XCUIApplication().collectionViews
-//        let cell = collectionViewsQuery.children(matching: .cell).element(boundBy: 3)
-//        cell.buttons["icon liked"].tap()
+//class Loop: UITestBase {
+//    
+//    override func setUp() {
+//        super.setUp()
 //        
-//        let cell2 = collectionViewsQuery.children(matching: .cell).element(boundBy: 2)
-//        cell2.staticTexts["Reply text"].swipeDown()
-//        
-//        let iconLikedButton = collectionViewsQuery.children(matching: .cell).element(boundBy: 4).buttons["icon liked"]
-//        iconLikedButton.tap()
-//        cell2.otherElements.containing(.staticText, identifier:"Reply text").children(matching: .other).element(boundBy: 0).children(matching: .button).element(boundBy: 0).swipeDown()
-//        cell2.buttons["icon liked"].tap()
-//        cell.staticTexts["Reply text"].swipeUp()
-//        cell.otherElements.containing(.staticText, identifier:"Reply text").children(matching: .other).element(boundBy: 0).children(matching: .button).element(boundBy: 0).swipeRight()
-//        collectionViewsQuery.children(matching: .cell).element(boundBy: 7).buttons["icon liked"].tap()
-//        iconLikedButton.tap()
-//        
-    }
-    
-}
-
+//    }
+//    
+//    override func tearDown() {
+//        super.tearDown()
+//    }
+//    
+//    func testAPIMock() {
+//        while true {}
+//    }
+//    
+//}
+//
