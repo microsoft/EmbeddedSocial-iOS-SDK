b2b2629a4e453d7887ccb36646a3e0d9925d3273
Oleksandr Kolesnyk <oleksandr.kolesnyk@akvelon.com>
2017-10-03 12:51:44 +0300

    Fixes

--- EmbeddedSocial/Sources/Controls/ActivityButtonCell/ActivityButtonCell.swift
+++ EmbeddedSocial/Sources/Controls/ActivityButtonCell/ActivityButtonCell.swift
@@ -1,9 +1,6 @@
 //
-//  ActivityButtonCell.swift
-//  EmbeddedSocial
-//
-//  Created by Mac User on 02.10.17.
-//  Copyright © 2017 Akvelon. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 //
 
 import UIKit
