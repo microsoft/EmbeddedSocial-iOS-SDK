049014a79a52cb37efc997dfaf1d4dbb9ca2c77f
Igor Popov <igor.popov@akvelon.com>
2017-10-04 10:53:45 +0300

    New mock added.

--- EmbeddedSocialTests/Modules/Activity/ActivityPresenterTests.swift
+++ EmbeddedSocialTests/Modules/Activity/ActivityPresenterTests.swift
@@ -6,55 +6,91 @@
 import XCTest
 @testable import EmbeddedSocial
 
-class ActivityInteractorMock: ActivityInteractorInput, DataSourceDelegate {
+
+class MockActivityInteractor: ActivityInteractorInput {
     
-    var didLoadPaths: [IndexPath]?
+    //MARK: - loadMyActivities
     
-    func didFail(error: Error) {
-        
+    var loadMyActivitiesCompletionCalled = false
+    var loadMyActivitiesResult: ActivityItemListResult!
+    
+    func loadMyActivities(completion: ((ActivityItemListResult) -> Void)?) {
+        loadMyActivitiesCompletionCalled = true
+        loadMyActivitiesResult?(loadMyActivitiesResult)
     }
     
-    func didLoad(indexPaths: [IndexPath]) {
-        didLoadPaths = indexPaths
+    //MARK: - loadNextPageMyActivities
+    
+    var loadNextPageMyActivitiesCompletionCalled = false
+    var loadNextPageMyActivitiesResult: ActivityItemListResult!
+    
+    func loadNextPageMyActivities(completion: ((ActivityItemListResult) -> Void)?) {
+        loadNextPageMyActivitiesCompletionCalled = true
+        completion?(loadNextPageMyActivitiesResult)
     }
     
-    var followingActivityItemsResult: Result<[ActionItem]>!
-    var pendingRequestsItemsResult: Result<[PendingRequestItem]>!
+    //MARK: - loadOthersActivities
     
-    func loadAll() {
-        
+    var loadOthersActivitiesCompletionCalled = false
+    var loadOthersActivitiesResult: ActivityItemListResult!
+    
+    func loadOthersActivities(completion: ((ActivityItemListResult) -> Void)?) {
+        loadOthersActivitiesCompletionCalled = true
+        completion?(loadOthersActivitiesResult)
     }
     
-    func loadNextPageMyActivities(completion: ((Result<[ActionItem]>) -> Void)? = nil) {
-        completion?(followingActivityItemsResult)
+    //MARK: - loadNextPageOthersActivities
+    
+    var loadNextPageOthersActivitiesCompletionCalled = false
+    var loadNextPageOthersActivitiesResult: ActivityItemListResult!
+    
+    func loadNextPageOthersActivities(completion: ((ActivityItemListResult) -> Void)?) {
+        loadNextPageOthersActivitiesCompletionCalled = true
+        completion?(loadNextPageOthersActivitiesResult)
     }
     
-    func loadNextPagePendigRequestItems(completion: ((Result<[PendingRequestItem]>) -> Void)? = nil) {
-        completion?(pendingRequestsItemsResult)
+    //MARK: - loadPendingRequestItems
+    
+    var loadPendingRequestItemsCompletionCalled = false
+    var loadPendingRequestResult: UserRequestListResult!
+    
+    func loadPendingRequestItems(completion: ((UserRequestListResult) -> Void)?) {
+        loadPendingRequestItemsCompletionCalled = true
+        completion?(loadPendingRequestResult)
+    }
+    
+    //MARK: - loadNextPagePendigRequestItems
+    
+    var loadNextPagePendigRequestItemsCompletionCalled = false
+    var loadNextPagePendigRequest: UserRequestListResult!
+    
+    func loadNextPagePendigRequestItems(completion: ((UserRequestListResult) -> Void)?) {
+        loadNextPagePendigRequestItemsCompletionCalled = true
+        completion?(loadNextPagePendigRequest)
+    }
+    
+    //MARK: - acceptPendingRequest
+    
+    var acceptPendingRequestUserCompletionCalled = false
+    var acceptPendingRequestUserCompletionReceivedArguments: (user: User, completion: (Result<Void>) -> Void)?
+    
+    func acceptPendingRequest(user: User, completion: @escaping (Result<Void>) -> Void) {
+        acceptPendingRequestUserCompletionCalled = true
+        acceptPendingRequestUserCompletionReceivedArguments = (user: user, completion: completion)
+    }
+    
+    //MARK: - rejectPendingRequest
+    
+    var rejectPendingRequestUserCompletionCalled = false
+    var rejectPendingRequestUserCompletionReceivedArguments: (user: User, completion: (Result<Void>) -> Void)?
+    
+    
+    func rejectPendingRequest(user: User, completion: @escaping (Result<Void>) -> Void) {
+        rejectPendingRequestUserCompletionCalled = true
+        rejectPendingRequestUserCompletionReceivedArguments = (user: user, completion: completion)
     }
     
 }
-
-class ActivityViewMock: ActivityViewInput {
-    
-    func setupInitialState() {
-        
-    }
-    
-    func registerCell(cell: UITableViewCell.Type, id: String) {
-        
-    }
-    
-    func showError(_ error: Error) {
-        
-    }
-    
-    func addNewItems(indexes: [IndexPath]) {
-        
-    }
-    
-}
-
 
 class ActivityPresenterTests: XCTestCase {
     
@@ -75,78 +111,6 @@ class ActivityPresenterTests: XCTestCase {
     
     override func tearDown() {
         super.tearDown()
-    }
-    
-    func testThatPedningDataSourceReturnsCorrectItems() {
-        
-        // given
-        let randomSectionIndex = Int(arc4random() % 5)
-        let itemsMockCount = Int(arc4random() % 10) + 1
-        let itemsMock = Array(0..<itemsMockCount).map { PendingRequestItem.mock(seed: $0) }
-        let resultMock: Result<[PendingRequestItem]> = .success(itemsMock)
-        
-        let header = SectionHeader(name: "", identifier: "")
-        let section = Section(model: header, items: [])
-        
-        let dataSource = MyPendingRequests(interactor: interactor,
-                                           section: section,
-                                           delegate: interactor,
-                                           index: randomSectionIndex)
-        
-        interactor.pendingRequestsItemsResult = resultMock
-        
-        // when
-        dataSource.loadMore()
-        
-        // then
-        XCTAssertTrue(dataSource.section.items.count == itemsMockCount)
-        XCTAssertTrue(interactor.didLoadPaths?.count == itemsMockCount)
-        for (index, path) in interactor.didLoadPaths!.enumerated() {
-            XCTAssertTrue(path.section == randomSectionIndex)
-        }
-    }
-    
-    func testThatMyFollowingActivityDataSourceReturnsCorrectItems() {
-        
-        // given
-        let itemsMock = Array(1..<5).map { ActionItem.mock(seed: $0) }
-        let resultMock: Result<[ActionItem]> = .success(itemsMock)
-        
-        let header = SectionHeader(name: "", identifier: "")
-        let section = Section(model: header, items: [])
-        let dataSource = MyActivities(interactor: interactor, section: section, index: 0)
-        
-        interactor.followingActivityItemsResult = resultMock
-        
-        // when
-        dataSource.loadMore()
-        
-        // then
-        XCTAssertTrue(dataSource.section.items.count == itemsMock.count)
-    }
-    
-    func testThatMyFeedActivityIsCorrect() {
-        
-        // given
-        
-        let itemsCountA = Int(arc4random() % 10) + 1
-        let itemsMockA = Array(0..<itemsCountA).map { PendingRequestItem.mock(seed: $0) }
-        let resultMockA: Result<[PendingRequestItem]> = .success(itemsMockA)
-        interactor.pendingRequestsItemsResult = resultMockA
-        
-        let itemsCountB = Int(arc4random() % 10) + 1
-        let itemsMockB = Array(0..<itemsCountB).map { ActionItem.mock(seed: $0) }
-        let resultMockB: Result<[ActionItem]> = .success(itemsMockB)
-        interactor.followingActivityItemsResult = resultMockB
-        
-        
-        // when
-        sut.state = .my
-        sut.loadMore()
-        
-        XCTAssertTrue(sut.numberOfSections() == 2, "must be 2 sections for my feed")
-        XCTAssertTrue(sut.numberOfItems(in: 0) == itemsCountA)
-        XCTAssertTrue(sut.numberOfItems(in: 1) == itemsCountB)
     }
     
     
