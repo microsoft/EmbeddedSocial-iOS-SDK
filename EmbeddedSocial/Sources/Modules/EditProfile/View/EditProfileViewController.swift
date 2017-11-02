06d37ab702015cb44ab993c1a9b3b51780015988
Vadim Bulavin <vadim.bulavin@akvelon.com>
2017-11-02 14:48:01 +0200

    Implemented

--- EmbeddedSocial/Sources/Modules/EditProfile/View/EditProfileViewController.swift
+++ EmbeddedSocial/Sources/Modules/EditProfile/View/EditProfileViewController.swift
@@ -39,7 +39,7 @@ extension EditProfileViewController: EditProfileViewInput {
     }
     
     func showError(_ error: Error) {
-        showErrorAlert(error)
+        showErrorAlert(error, ignoreNoConnectionErrors: false)
     }
     
     func setSaveButtonEnabled(_ isEnabled: Bool) {
